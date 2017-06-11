c6a7266e12457c79ee47f28a39a2566446eaa734
Jose Barrios <josek777@gmail.com>
2017-06-11 16:45:29 -0400

    adding 259 problem

---
+++ 259 capicuaNumbers.js
@@ -0,0 +1,12 @@
+function findCapicuas(n) {
+    let cont = 0, start = Math.pow(10,n-1) == 1 ? 0 : Math.pow(10,n-1) , end = Math.pow(10,n)
+    for(let i = start; i < end; i++) {
+       let reverseNumber = i.toString().split('').reverse().join('')
+        if(reverseNumber == i) {
+            cont++
+        }
+    }
+    return cont
+}
+
+findCapicuas(5)
