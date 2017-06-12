ae5dc3e0bc31102a9cda7f641628998c03e5b417
Jose Barrios <josek777@gmail.com>
2017-06-11 22:18:43 -0400

    añadiendo nuevas pruebas

--- 259 capicuaNumbers.js
+++ 259 capicuaNumbers.js
@@ -2,11 +2,12 @@ function findCapicuas(n) {
     let cont = 0, start = Math.pow(10,n-1) == 1 ? 0 : Math.pow(10,n-1) , end = Math.pow(10,n)
     for(let i = start; i < end; i++) {
        let reverseNumber = i.toString().split('').reverse().join('')
-        if(reverseNumber == i) {
+       if(reverseNumber == i) {
             cont++
         }
     }
     return cont
 }
-
+findCapicuas(1)
+findCapicuas(2)
 findCapicuas(5)
